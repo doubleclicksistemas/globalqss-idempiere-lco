d448b569716a9b8f1ca845531198bc629a4e7d3a
argenis <argenismrvel@gmail.com>
2024-10-16 23:56:31 -0400

    Add constants

--- org.globalqss.idempiere.LCO.withholdings/src/com/ingeint/utils/IngeintConstants.java
+++ org.globalqss.idempiere.LCO.withholdings/src/com/ingeint/utils/IngeintConstants.java
@@ -1,5 +1,10 @@
 package com.ingeint.utils;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import org.compiere.model.PO;
+
 public class IngeintConstants {
 	
 	//-----------------------------------------------Column Names------------------------------------------------------------------
@@ -8,10 +13,17 @@ public class IngeintConstants {
 	public static final String COLUMNNAME_ConvertedTaxAmt = "ConvertedTaxAmt";
 	public static final String COLUMNNAME_C_CurrencyTo_ID = "C_CurrencyTo_ID";
 	public static final String COLUMNNAME_ConversionRate = "ConversionRate";
+	public static final String COLUMNNAME_WriteOffInvoice = "WriteOffInvoice";
+	public static final String COLUMNNAME_WriteOffPayment = "WriteOffPayment";
 	
 	//--------------------------------------------------Sys Configs----------------------------------------------------------------
 	public static final String SYSCONFIG_LVE_GENERATE_WITHHOLDINGS_WITH_LIST_PRECISION = "LVE_GENERATE_WITHHOLDINGS_WITH_LIST_PRECISION";
 	public static final String SYSCONFIG_LVE_DROP_ALLOCATION_DIFFERENCIAL = "LVE_DROP_ALLOCATION_DIFFERENCIAL";
 	public static final String SYSCONFIG_PRECISION_RATE = "PRECISION_RATE";
 	public static final String SYSCONFIG_POST_DOCUMENT_DATE = "POST_DOCUMENT_DATE";
+	
+	public static BigDecimal getvalueAsBigDecimal(PO po, String columnName) {
+		return Optional.ofNullable((BigDecimal) po.get_Value(columnName))
+				.orElse(BigDecimal.ZERO);
+	}
 }
